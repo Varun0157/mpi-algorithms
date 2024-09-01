2d22189bd8b428f0f1ddd28b3fb667c84abd0089
Varun0157 <varun.edachali@gmail.com>
2024-09-02 02:47:21 +0530

    misc: correct gitignore, make q1 tests more uniform with the rest

--- 1/test.sh
+++ 1/test.sh
@@ -4,15 +4,15 @@
 mpicxx 1.cpp
 
 # create a random test case
-# python create.py
-# if [ $? -ne 0 ]; then
-#     echo "failed to create test case"
-#     exit 1
-# fi
+python create.py
+if [ $? -ne 0 ]; then
+    echo "failed to create test case"
+    exit 1
+fi
 
 # for num in range 1 to 12, call ../test.sh 'mpiexec -n ${num} ./a.out inp1.txt' 'inp1-opt.txt'
 for num in {1..12}
 do
     echo $num 
-    bash ../test-file.sh "mpiexec -n ${num} ./a.out 1000-1000-850.txt" "1000-1000-850-opt.txt"
+    bash ../test-file.sh "mpiexec -n ${num} ./a.out random.txt" "random-opt.txt"
 done

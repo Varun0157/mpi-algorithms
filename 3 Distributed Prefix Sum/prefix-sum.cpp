b42dfde5e771b4f8e1e5b1c3d13f639fda5fd0ae
Varun0157 <varun.edachali@gmail.com>
2024-12-14 15:42:37 +0530

    3: [refactor] modularise

--- 3 Distributed Prefix Sum/prefix-sum.cpp
+++ 3 Distributed Prefix Sum/prefix-sum.cpp
@@ -10,6 +10,11 @@ Distributed prefix sum
 #include <mpi.h>
 #include <vector>
 
+int finalise() {
+  MPI_Finalize();
+  return 0;
+}
+
 void getNumbers(const char *filename, int &N, std::vector<double> &numbers) {
   std::ifstream input(filename);
   if (!input) {
@@ -46,63 +51,15 @@ void sendLocalNumbersFromZero(const int NUM_PROC, const int N,
   }
 }
 
-void storePrefixSums(std::vector<double> &nums) {
+void storeLocalPrefixSums(std::vector<double> &nums) {
   for (int i = 1; i < nums.size(); i++)
     nums[i] += nums[i - 1];
 }
 
-std::vector<double> accumulateLocalSums(const std::vector<double> &currLocal,
-                                        const int N, const int NUM_PROC) {
-  std::vector<double> globalNums(N);
-  for (int i = 0; i < currLocal.size(); i++)
-    globalNums[i] = currLocal[i];
-
-  for (int i = 1; i < NUM_PROC; i++) {
-    const auto [qStart, qEnd] = getBounds(i, NUM_PROC, N);
-    MPI_Recv(globalNums.data() + qStart, qEnd - qStart, MPI_DOUBLE, i, 0,
-             MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-  }
-
-  return globalNums;
-}
-
-int main(int argc, char *argv[]) {
-  if (argc != 2) {
-    std::cerr << "usage: " << argv[0] << " <input file>" << std::endl;
-    return 1;
-  }
-
-  std::cout << std::setprecision(10);
-
-  MPI_Init(&argc, &argv);
-
-  int WORLD_SIZE;
-  MPI_Comm_size(MPI_COMM_WORLD, &WORLD_SIZE);
-
-  int WORLD_RANK;
-  MPI_Comm_rank(MPI_COMM_WORLD, &WORLD_RANK);
-
-  int N;
-  std::vector<double> numbers;
-  if (WORLD_RANK == 0) {
-    getNumbers(argv[1], N, numbers);
-  }
-
-  std::chrono::steady_clock::time_point beginTime =
-                                            std::chrono::steady_clock::now(),
-                                        endTime;
-
-  // broadcast the number of elements to all processes
-  MPI_Bcast(&N, 1, MPI_INT, 0, MPI_COMM_WORLD);
-
-  const int NUM_PROC = std::min(WORLD_SIZE, N);
-
-  std::vector<double> localNums;
-
+void setLocalNums(const int WORLD_RANK, const int WORLD_SIZE, const int N,
+                  const int NUM_PROC, std::vector<double> &numbers,
+                  std::vector<double> &localNums) {
   const auto [start, end] = getBounds(WORLD_RANK, NUM_PROC, N);
-
-  if (WORLD_RANK >= NUM_PROC)
-    goto finalise;
 
   // store local numbers for each process to handle
   if (WORLD_RANK == 0) {
@@ -116,9 +73,10 @@ int main(int argc, char *argv[]) {
     MPI_Recv(localNums.data(), end - start, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD,
              MPI_STATUS_IGNORE);
   }
+}
 
-  storePrefixSums(localNums);
-
+void addWeightFromPreviousSets(const int WORLD_RANK, const int NUM_PROC,
+                               std::vector<double> &localNums) {
   if (WORLD_RANK != 0) {
     // send closing value to rank 0
     MPI_Send(&localNums.back(), 1, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD);
@@ -148,30 +106,87 @@ int main(int argc, char *argv[]) {
     for (int i = 1; i < NUM_PROC; i++)
       MPI_Send(&closingNums[i - 1], 1, MPI_DOUBLE, i, 0, MPI_COMM_WORLD);
   }
+}
 
+std::chrono::time_point<std::chrono::steady_clock>
+accumulateLocalSums(const int WORLD_RANK, const int NUM_PROC, const int N,
+                    std::vector<double> &localNums,
+                    std::vector<double> &globalNums) {
   // send the localNums back to rank 0 and accumulate
   if (WORLD_RANK != 0) {
     MPI_Send(localNums.data(), localNums.size(), MPI_DOUBLE, 0, 0,
              MPI_COMM_WORLD);
   } else {
-    std::vector<double> globalNums =
-        accumulateLocalSums(localNums, N, NUM_PROC);
+    globalNums.resize(N);
+    for (int i = 0; i < localNums.size(); i++)
+      globalNums[i] = localNums[i];
 
-    endTime = std::chrono::steady_clock::now();
-
-    std::ofstream output(std::to_string(N) + "_time.txt", std::ios::app);
-    output << WORLD_SIZE << ":"
-           << std::chrono::duration_cast<std::chrono::nanoseconds>(endTime -
-                                                                   beginTime)
-                  .count()
-           << std::endl;
-
-    for (const double &num : globalNums)
-      std::cout << num << " ";
-    std::cout << std::endl;
+    for (int i = 1; i < NUM_PROC; i++) {
+      const auto [qStart, qEnd] = getBounds(i, NUM_PROC, N);
+      MPI_Recv(globalNums.data() + qStart, qEnd - qStart, MPI_DOUBLE, i, 0,
+               MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+    }
   }
 
-finalise:
-  MPI_Finalize();
-  return 0;
+  return std::chrono::steady_clock::now();
+}
+
+int main(int argc, char *argv[]) {
+  if (argc != 2) {
+    std::cerr << "usage: " << argv[0] << " <input file>" << std::endl;
+    return 1;
+  }
+
+  std::cout << std::setprecision(10);
+
+  MPI_Init(&argc, &argv);
+
+  int WORLD_SIZE;
+  MPI_Comm_size(MPI_COMM_WORLD, &WORLD_SIZE);
+
+  int WORLD_RANK;
+  MPI_Comm_rank(MPI_COMM_WORLD, &WORLD_RANK);
+
+  int N;
+  std::vector<double> numbers;
+  if (WORLD_RANK == 0) {
+    getNumbers(argv[1], N, numbers);
+  }
+
+  std::chrono::steady_clock::time_point beginTime =
+      std::chrono::steady_clock::now();
+
+  // broadcast the number of elements to all processes
+  MPI_Bcast(&N, 1, MPI_INT, 0, MPI_COMM_WORLD);
+
+  const int NUM_PROC = std::min(WORLD_SIZE, N);
+
+  std::vector<double> localNums;
+
+  if (WORLD_RANK >= NUM_PROC)
+    return finalise();
+
+  setLocalNums(WORLD_RANK, WORLD_SIZE, N, NUM_PROC, numbers, localNums);
+  storeLocalPrefixSums(localNums);
+  addWeightFromPreviousSets(WORLD_RANK, NUM_PROC, localNums);
+
+  std::vector<double> globalNums;
+  auto endTime =
+      accumulateLocalSums(WORLD_RANK, NUM_PROC, N, localNums, globalNums);
+
+  if (WORLD_RANK != 0)
+    return finalise();
+
+  std::ofstream output(std::to_string(N) + "_time.txt", std::ios::app);
+  output << WORLD_SIZE << ":"
+         << std::chrono::duration_cast<std::chrono::nanoseconds>(endTime -
+                                                                 beginTime)
+                .count()
+         << std::endl;
+
+  for (const double &num : globalNums)
+    std::cout << num << " ";
+  std::cout << std::endl;
+
+  return finalise();
 }

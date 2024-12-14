fa12c60239b2bca95b74095ec4bf06d124e60fad
Varun0157 <varun.edachali@gmail.com>
2024-12-14 15:30:41 +0530

    1: [refactor] heavily modularise

--- 1 K Nearest Neighbours/nearest-neighbours.cpp
+++ 1 K Nearest Neighbours/nearest-neighbours.cpp
@@ -12,6 +12,11 @@ Distributed K Nearest Neighbours
 #include <queue>
 #include <vector>
 
+int finalise() {
+  MPI_Finalize();
+  return 0;
+}
+
 void getPointsAndQueries(const char *filename, int &N, int &M, int &K,
                          std::vector<std::pair<double, double>> &points,
                          std::vector<std::pair<double, double>> &queries) {
@@ -48,7 +53,10 @@ std::pair<int, int> getBounds(int rank, int NUM_PROC, int M) {
 std::vector<std::vector<std::pair<double, double>>>
 getLocalNearestNeighbours(std::vector<std::pair<double, double>> &points,
                           std::vector<std::pair<double, double>> &localQueries,
-                          int start, int end, int pointsPerQuery) {
+                          const int WORLD_RANK, const int NUM_PROC, const int M,
+                          int pointsPerQuery) {
+  const auto [start, end] = getBounds(WORLD_RANK, NUM_PROC, M);
+
   std::vector<std::vector<std::pair<double, double>>> localNN(end - start);
   for (int i = 0; i < localQueries.size(); i++) {
     const std::pair<double, double> &query = localQueries[i];
@@ -94,6 +102,53 @@ void sendLocalQueries(std::vector<std::pair<double, double>> &queries,
   }
 }
 
+void setLocalQueries(const int WORLD_RANK, const int WORLD_SIZE, const int M,
+                     const int NUM_PROC,
+                     std::vector<std::pair<double, double>> &queries,
+                     std::vector<std::pair<double, double>> &localQueries) {
+  const auto [start, end] = getBounds(WORLD_RANK, NUM_PROC, M);
+
+  // gather local queries for each process to handle
+  if (WORLD_RANK == 0) {
+    sendLocalQueries(queries, NUM_PROC, M, 0);
+
+    localQueries = std::vector<std::pair<double, double>>(
+        queries.begin() + start, queries.begin() + end);
+    queries.clear();
+  } else {
+    localQueries.resize(end - start);
+    MPI_Recv(localQueries.data(), (end - start) * 2, MPI_DOUBLE, 0, 0,
+             MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+  }
+}
+
+std::chrono::time_point<std::chrono::steady_clock> accumulateNearestNeighbours(
+    const int WORLD_RANK, const int WORLD_SIZE, const int NUM_PROC, const int M,
+    const int pointsPerQuery,
+    std::vector<std::vector<std::pair<double, double>>> &localNN,
+    std::vector<std::vector<std::pair<double, double>>> &globalNN) {
+  // send and accumulate local nearest neighbours at rank 0
+  if (WORLD_RANK != 0) {
+    for (int i = 0; i < localNN.size(); i++)
+      MPI_Send(localNN[i].data(), pointsPerQuery * 2, MPI_DOUBLE, 0, 0,
+               MPI_COMM_WORLD);
+  } else {
+    // push the current local nearest neighbours (rank 0)
+    for (int i = 0; i < localNN.size(); i++)
+      globalNN[i] = localNN[i];
+
+    // receive the nearest neighbours from other processes
+    for (int i = 1; i < NUM_PROC; i++) {
+      const auto [qStart, qEnd] = getBounds(i, NUM_PROC, M);
+      for (int j = qStart; j < qEnd; j++)
+        MPI_Recv(globalNN[j].data(), pointsPerQuery * 2, MPI_DOUBLE, i, 0,
+                 MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+    }
+  }
+
+  return std::chrono::steady_clock::now();
+}
+
 int main(int argc, char *argv[]) {
   if (argc != 2) {
     std::cerr << "usage: " << argv[0] << " <input file>" << std::endl;
@@ -117,8 +172,7 @@ int main(int argc, char *argv[]) {
   }
 
   std::chrono::steady_clock::time_point beginTime =
-                                            std::chrono::steady_clock::now(),
-                                        endTime;
+      std::chrono::steady_clock::now();
 
   // broadcast N, M, K and all points to all processes
   MPI_Bcast(&N, 1, MPI_INT, 0, MPI_COMM_WORLD);
@@ -134,70 +188,41 @@ int main(int argc, char *argv[]) {
   std::vector<std::pair<double, double>> localQueries;
   std::vector<std::vector<std::pair<double, double>>> localNN;
 
-  const auto [start, end] = getBounds(WORLD_RANK, NUM_PROC, M);
   const int pointsPerQuery = std::min(K, N);
 
   if (WORLD_RANK >= NUM_PROC)
-    goto finalise;
+    return finalise();
 
-  // gather local queries for each process to handle
-  if (WORLD_RANK == 0) {
-    sendLocalQueries(queries, NUM_PROC, M, 0);
+  setLocalQueries(WORLD_RANK, WORLD_SIZE, M, NUM_PROC, queries, localQueries);
+  localNN = getLocalNearestNeighbours(points, localQueries, WORLD_RANK,
+                                      NUM_PROC, M, pointsPerQuery);
 
-    localQueries = std::vector<std::pair<double, double>>(
-        queries.begin() + start, queries.begin() + end);
-    queries.clear();
-  } else {
-    localQueries.resize(end - start);
-    MPI_Recv(localQueries.data(), (end - start) * 2, MPI_DOUBLE, 0, 0,
-             MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+  std::vector<std::vector<std::pair<double, double>>> globalNN(
+      M, std::vector<std::pair<double, double>>(pointsPerQuery));
+  auto endTime = accumulateNearestNeighbours(
+      WORLD_RANK, WORLD_SIZE, NUM_PROC, M, pointsPerQuery, localNN, globalNN);
+
+  if (WORLD_RANK != 0)
+    return finalise();
+
+  endTime = std::chrono::steady_clock::now();
+
+  // create a file named {N}_{M}_{K}_time.txt
+  std::string fileName = std::to_string(N) + "_" + std::to_string(M) + "_" +
+                         std::to_string(K) + "_time.txt";
+  std::ofstream output(fileName, std::ios::app);
+  output << WORLD_SIZE << ":"
+         << std::chrono::duration_cast<std::chrono::nanoseconds>(endTime -
+                                                                 beginTime)
+                .count()
+         << std::endl;
+
+  // print out the nearest neighbours
+  for (int i = 0; i < M; i++) {
+    for (int j = 0; j < globalNN[i].size(); j++)
+      std::cout << globalNN[i][j].first << " " << globalNN[i][j].second
+                << std::endl;
   }
 
-  localNN = getLocalNearestNeighbours(points, localQueries, start, end,
-                                      pointsPerQuery);
-
-  // send and accumulate local nearest neighbours at rank 0
-  if (WORLD_RANK != 0) {
-    for (int i = 0; i < localNN.size(); i++)
-      MPI_Send(localNN[i].data(), pointsPerQuery * 2, MPI_DOUBLE, 0, 0,
-               MPI_COMM_WORLD);
-  } else {
-    std::vector<std::vector<std::pair<double, double>>> globalNN(
-        M, std::vector<std::pair<double, double>>(pointsPerQuery));
-
-    // push the current local nearest neighbours (rank 0)
-    for (int i = 0; i < localNN.size(); i++)
-      globalNN[i] = localNN[i];
-
-    // receive the nearest neighbours from other processes
-    for (int i = 1; i < NUM_PROC; i++) {
-      const auto [qStart, qEnd] = getBounds(i, NUM_PROC, M);
-      for (int j = qStart; j < qEnd; j++)
-        MPI_Recv(globalNN[j].data(), pointsPerQuery * 2, MPI_DOUBLE, i, 0,
-                 MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-    }
-
-    endTime = std::chrono::steady_clock::now();
-
-    // create a file named {N}_{M}_{K}_time.txt
-    std::string fileName = std::to_string(N) + "_" + std::to_string(M) + "_" +
-                           std::to_string(K) + "_time.txt";
-    std::ofstream output(fileName, std::ios::app);
-    output << WORLD_SIZE << ":"
-           << std::chrono::duration_cast<std::chrono::nanoseconds>(endTime -
-                                                                   beginTime)
-                  .count()
-           << std::endl;
-
-    // print out the nearest neighbours
-    for (int i = 0; i < M; i++) {
-      for (int j = 0; j < globalNN[i].size(); j++)
-        std::cout << globalNN[i][j].first << " " << globalNN[i][j].second
-                  << std::endl;
-    }
-  }
-
-finalise:
-  MPI_Finalize();
-  return 0;
+  return finalise();
 }
